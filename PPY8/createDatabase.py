ab6d23f33943f8fccf6571f95d9a8e7f053cce0c
user <s25847@pjwstk.edu.pl>
2023-05-31 11:52:57 +0200

    another adjustment

--- PPY8/createDatabase.py
+++ PPY8/createDatabase.py
@@ -4,30 +4,37 @@ db = sqlite3.connect("studenci.db")
 cursor = db.cursor()
 
 cursor.execute('''
-    CREATE TABLE Ocena (
-        IdStudent int NOT NULL,
-        IdPrzedmiot int NOT NULL,
-        Ocena varchar(3) NOT NULL,
-        CONSTRAINT Ocena_pk PRIMARY KEY (IdStudent,IdPrzedmiot),
-        CONSTRAINT Ocena_Student FOREIGN KEY (IdStudent)
-        REFERENCES Student (IdStudent),
-        CONSTRAINT Ocena_Przedmiot FOREIGN KEY (IdPrzedmiot)
-        REFERENCES Przedmiot (IdPrzedmiot)
+    CREATE TABLE "Ocena" (
+	    "IdOcena"	INTEGER NOT NULL,
+	    "IdStudent"	INTEGER NOT NULL,
+	    "IdPrzedmiot"	INTEGER NOT NULL,
+	    "Ocena"	TEXT NOT NULL,
+	    CONSTRAINT "Ocena_pk" PRIMARY KEY("IdOcena" AUTOINCREMENT),
+	    CONSTRAINT "Ocena_Student" FOREIGN KEY("IdStudent") REFERENCES "Student"("IdStudent"),
+	    CONSTRAINT "Ocena_Przedmiot" FOREIGN KEY("IdPrzedmiot") REFERENCES "Przedmiot"("IdPrzedmiot")
+)
     );
 ''')
 
 cursor.execute('''
-    CREATE TABLE Przedmiot (
-        IdPrzedmiot int NOT NULL CONSTRAINT Przedmiot_pk PRIMARY KEY,
-        Nazwa varchar(30) NOT NULL
-    );
+    CREATE TABLE "Przedmiot" (
+	    "IdPrzedmiot"	INTEGER NOT NULL,
+	    "Nazwa"	TEXT NOT NULL,
+	    CONSTRAINT "Przedmiot_pk" PRIMARY KEY("IdPrzedmiot" AUTOINCREMENT)
+)
 ''')
 cursor.execute('''
     CREATE TABLE Student (
-        IdStudent double precision NOT NULL CONSTRAINT Student_pk PRIMARY KEY,
-        Imie varchar(30) NOT NULL,
-        Nazwisko varchar(30) NOT NULL
+        CREATE TABLE "Student" (
+	    "IdStudent"	INTEGER NOT NULL,
+	    "Imie"	TEXT NOT NULL,
+	    "Nazwisko"	TEXT NOT NULL,
+	    CONSTRAINT "Student_pk" PRIMARY KEY("IdStudent" AUTOINCREMENT)
+)
     );
+''')
+cursor.execute('''
+    CREATE TABLE sqlite_sequence(name,seq)
 ''')
 db.commit()
 db.close()
